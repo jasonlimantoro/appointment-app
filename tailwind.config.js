e0be457ba20a8ea7b6654a6bf3832460a09d04ef
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-05-20 21:58:13 +0700

    CONFIG(tailwind): add theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          ...colors.blue,
+          default: '#1985a0',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
