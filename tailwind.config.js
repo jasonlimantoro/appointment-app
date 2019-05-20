28fbf48f4a864a5be4003fa037eeabdba2240aa5
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-05-20 22:44:30 +0700

    CONFIG(tailwind): add shades to color blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
       colors: {
         blue: {
           ...colors.blue,
+          light: '#19A8C3',
           default: '#1985a0',
+          dark: '#095f7d',
         },
       },
     },
