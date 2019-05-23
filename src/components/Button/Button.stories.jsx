527e3576584793de77960abe47f53e9e14b0d934
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-05-23 08:47:12 +0700

    DOCS(stories): restructure stories

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -7,9 +7,25 @@ const TEXT = {
   long: 'Some text that is very long until the button is very long',
 };
 
-storiesOf('Component|Button/default', module)
-  .add('basic', () => <Button ripple={false}>{TEXT.short}</Button>)
-  .add('ripple', () => <Button>{TEXT.short}</Button>)
+storiesOf('Component|Button', module).add('variants', () => {
+  const themedButton = [
+    { name: 'default', text: TEXT.short },
+    { name: 'primary', text: TEXT.short },
+  ];
+  return (
+    <div className="flex">
+      {themedButton.map(b => (
+        <div key={b.name} className="flex flex-col mx-3">
+          <Button className={b.name}>{b.text}</Button>
+          {b.name}
+        </div>
+      ))}
+    </div>
+  );
+});
+
+storiesOf('Component|Button/layouts', module)
+  .add('inline', () => <Button>{TEXT.short}</Button>)
   .add('full width', () => (
     <div className="w-1/2 border-2 border-red-800">
       Parent container
@@ -17,18 +33,6 @@ storiesOf('Component|Button/default', module)
     </div>
   ));
 
-storiesOf('Component|Button/primary', module)
-  .add('basic', () => (
-    <Button variant="primary" ripple={false}>
-      {TEXT.short}
-    </Button>
-  ))
-  .add('ripple', () => <Button variant="primary">{TEXT.short}</Button>)
-  .add('full width', () => (
-    <div className="w-1/2 border-2 border-red-800">
-      Parent container
-      <Button variant="primary" fullWidth>
-        {TEXT.short}
-      </Button>
-    </div>
-  ));
+storiesOf('Component|Button/others', module).add('disable ripple', () => (
+  <Button ripple={false}>{TEXT.short}</Button>
+));
--- src/components/Card/Card.stories.jsx
+++ src/components/Card/Card.stories.jsx
@@ -2,21 +2,31 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Card from '.';
 
-storiesOf('Component|Card/default', module)
-  .add('inline', () => <Card>Some Text</Card>)
-  .add('full width', () => (
-    <div className="border-2 border-red-500">
-      Parent Element <Card fullWidth>Some Text</Card>
-    </div>
-  ));
+const TEXT = {
+  short: 'Some text',
+  long: 'Some longer text inside a component',
+};
 
-storiesOf('Component|Card/primary', module)
-  .add('inline', () => <Card variant="primary">Some Text</Card>)
+storiesOf('Component|Card', module).add('variants', () => {
+  const themedButton = [
+    { name: 'default', text: TEXT.short },
+    { name: 'primary', text: TEXT.short },
+  ];
+  return (
+    <div className="flex">
+      {themedButton.map(b => (
+        <div key={b.name} className="flex flex-col mx-3">
+          <Card className={b.name}>{b.text}</Card>
+          {b.name}
+        </div>
+      ))}
+    </div>
+  );
+});
+storiesOf('Component|Card/layouts', module)
+  .add('inline', () => <Card>{TEXT.short}</Card>)
   .add('full width', () => (
     <div className="border-2 border-red-500">
-      Parent Element{' '}
-      <Card variant="primary" fullWidth>
-        Some Text
-      </Card>
+      Parent Element <Card fullWidth>{TEXT.short}</Card>
     </div>
   ));
