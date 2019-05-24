049af637398cf3c6d6e7754535695ca7688bc206
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-05-24 12:13:36 +0700

    FEAT(Logout Page): implement logout page

--- src/routes/Main/SignOut.jsx
+++ src/routes/Main/SignOut.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Master } from '../../components/layouts';
+import { Button, Input } from '../../components';
 
 const SignOut = () => {
-  return <h1>Sign Out Guest</h1>;
+  return (
+    <Master
+      layoutProps={{ headerProps: { title: 'Company Name' } }}
+      title="Enter Guest Details to Sign Out"
+    >
+      <form action="#" className="flex flex-col w-1/2">
+        <Input
+          fullWidth
+          label="Name"
+          inputProps={{ placeholder: 'Enter name' }}
+          className="mb-8"
+        />
+        <Button variant="primary">Sign Out</Button>
+      </form>
+    </Master>
+  );
 };
 
 SignOut.propTypes = {};
