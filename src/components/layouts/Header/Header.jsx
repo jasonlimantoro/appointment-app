5bb208effa62c86ba5f6cd5ce8789e9c67d68c34
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-05-26 02:22:38 +0700

    FEAT(Header): add logout Button in header

--- src/components/layouts/Header/Header.jsx
+++ src/components/layouts/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '../../Button/Button';
 
 const Header = ({ title }) => {
   return (
     <div className="w-full bg-blue-dark flex justify-center items-center text-white min-h-header h3 my-0">
-      {title}
+      <div>{title}</div>
+      <div className="absolute right-0 h-header flex items-center">
+        <Button variant="transparentLight" className="text-sm">
+          Logout
+        </Button>
+      </div>
     </div>
   );
 };
