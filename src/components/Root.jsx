1e09f23910f16241adcc8a18c44d20b3f5e9f8e1
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-05-27 13:28:19 +0700

    CONFIG: boilerplate for graphql and aws-amplify

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import AWSAppSyncClient from 'aws-appsync';
+import { ApolloProvider } from 'react-apollo';
 import PropTypes from 'prop-types';
 import configureStore from '../store';
+import config from '../aws-exports';
 import App from './App';
+
+const client = new AWSAppSyncClient({
+  url: config.aws_appsync_graphqlEndpoint,
+  region: config.aws_appsync_region,
+  auth: {
+    type: config.aws_appsync_authenticationType,
+    apiKey: config.aws_appsync_apiKey,
+  },
+});
 
 const defaultStore = configureStore();
 
 const Root = ({ store }) => {
   return (
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
   );
 };
 
