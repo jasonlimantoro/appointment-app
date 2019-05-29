ce5084f7f06652920eadadaaf84d81b2b2805884
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-05-29 09:23:14 +0700

    REFACTOR(service): use the API from 'aws-amplify'

--- src/services/aws/api/guest/GuestService.js
+++ src/services/aws/api/guest/GuestService.js
@@ -1,27 +1,15 @@
-import gql from 'graphql-tag';
+import { API } from 'aws-amplify';
 
 import BaseService from '../../../BaseService';
-import { client } from '../../../../config';
+import { listGuests } from '../../../../graphql/queries';
 import { getNestedObjectValue } from '../../../../components/utils/helpers';
 
-const listGuest = gql`
-  query getListGuest {
-    listGuests {
-      items {
-        id
-        firstName
-        lastName
-        email
-        NIK
-        company
-      }
-    }
-  }
-`;
-
 export default class GuestService extends BaseService {
-  list = async (gqlQuery = listGuest) => {
-    const res = await client.query({ query: gqlQuery });
+  list = async (gqlQuery = listGuests, { variables } = {}) => {
+    const res = await API.graphql({
+      query: gqlQuery,
+      variables,
+    });
     return getNestedObjectValue(res, [])(['data', 'listGuests', 'items']);
   };
 }
